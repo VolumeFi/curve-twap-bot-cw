5b1abef0bad540d3ebc892c5228981d292477bb9
webelf101 <m63974529@gmail.com>
2024-01-09 01:16:06 +0800

    update and bugfix

--- src/state.rs
+++ src/state.rs
@@ -2,14 +2,16 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use crate::msg::Metadata;
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Timestamp};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
+    pub retry_delay: u64,
     pub job_id: String,
     pub owner: Addr,
     pub metadata: Metadata,
 }
 
+pub const WITHDRAW_TIMESTAMP: Map<(u32, u32), Timestamp> = Map::new("withdraw_timestamp");
 pub const STATE: Item<State> = Item::new("state");
